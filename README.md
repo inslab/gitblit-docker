1fadc4643c6b12b18d0e0045743f02d16ace5971
minchulahn <minchulahn@inslab.co.kr>
2017-05-11 17:28:38 +0900

    Change ldap setting

--- README.md
+++ README.md
@@ -2,8 +2,9 @@ gitblit-docker
 ==============
 Environmental Variables
 -----------------------
-LDAP_URI : URI of LDAP server (ex : ldap://test.com:389)
 
-LDAP_BASE_DN : Base DN (ex : dc=test,dc=com)
+LDAP_HOST : Host of LDAP server (ex : 111.222.333.444)
 
-JAVA_OPTS : Java options to run gitblit (ex : -Xms512m -Xmx512m)
+LDAP_PORT : Port of LDAP service
+
+LDAP_BASE_DN : Base DN (ex : dc=example,dc=co)
--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 
 if [ ! -f "/opt/gitblit-data/gitblit.properties" ]; then
     cp -rf /tmp/gitblit-data/* /opt/gitblit-data
-    if [ -n "$LDAP_URI" ] && [ -n "$LDAP_BASE_DN" ]
+    if [ -n "$LDAP_HOST" ] && [ -n "$LDAP_PORT" ] && [ -n "$LDAP_BASE_DN" ]
         then
         echo "--> LDAP setting"
         sed -e 's%^realm.authenticationProviders =$%realm.authenticationProviders = ldap%' \
-            -e 's%^realm.ldap.server = ldap://localhost%realm.ldap.server = '"$LDAP_URI"'%' \
+            -e 's%^realm.ldap.server = ldap://localhost%realm.ldap.server = ldap://'"$LDAP_HOST"':'"$LDAP_PORT"'%' \
             -e 's%^realm.ldap.accountBase = OU=Users,OU=UserControl,OU=MyOrganization,DC=MyDomain$%realm.ldap.accountBase = '"$LDAP_BASE_DN"'%' \
-            -e 's%^realm.ldap.accountPattern = (&(objectClass=person)(sAMAccountName=${username}))%realm.ldap.accountPattern = (\&(objectClass=posixAccount)(uid=${username}))%' \
+            -e 's%^realm.ldap.accountPattern = (&(objectClass=person)(sAMAccountName=${username}))%realm.ldap.accountPattern = (\&(objectClass=person)(uid=${username}))%' \
             -e 's%^realm.ldap.username = cn=Directory Manager$%realm.ldap.username =%' \
             -e 's%^realm.ldap.password = password$%realm.ldap.password =%' \
             /tmp/gitblit-data/gitblit.properties > /opt/gitblit-data/gitblit.properties
@@ -20,4 +20,3 @@ fi
 
 cd /opt/gitblit
 java -server -Djava.awt.headless=true -jar /opt/gitblit/gitblit.jar --baseFolder /opt/gitblit-data
-
